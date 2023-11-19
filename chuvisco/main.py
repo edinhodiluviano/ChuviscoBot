05b1d4f392a63cc7bbcf5086213c234d73475e23
Edinho <git@final04.com>
2023-11-19 13:44:01 -0300

    feat: Add commands

--- chuvisco/main.py
+++ chuvisco/main.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import random
 
 import telegram
 import telegram.ext
@@ -24,6 +25,46 @@ async def save(
         session.commit()
 
 
+async def hacker(
+    update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE
+) -> None:
+    """Send a message when the command /hacker is issued."""
+
+    m = (
+        'Quer saber o que é um hacker, o que ele faz e como se tornar um?!\n'
+        'aqui é um bom começo: https://garoa.net.br/wiki/Hacker'
+    )
+    await update.message.reply_text(m)
+
+
+async def hackear(
+    update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE
+) -> None:
+    """Send a message when the command /haquear + "something" is issued."""
+
+    words = update.message.text.split(' ', 1)
+    if len(words) == 1:
+        this_folder = os.path.dirname(os.path.realpath(__file__))
+        file = this_folder + '/' + 'hackear_algo.txt'
+        with open(file) as f:
+            message = f.read()
+    else:
+        videos = [
+            'https://www.youtube.com/watch?v=EErY75MXYXI',
+            'https://www.youtube.com/watch?v=CNuzEXIAxe8',
+            'https://www.youtube.com/watch?v=9_f9Mr10IeI',
+            'https://www.youtube.com/watch?v=1MGlTgSnsE4',
+            'https://www.youtube.com/watch?v=79vdbA0kth4',
+            'https://www.youtube.com/watch?v=AjWfY7SnMBI',
+        ]
+        video = random.choice(videos)
+        message = (
+            'Para isso vc precisa decifrar a mensagem secreta escondida '
+            f'nesse video: {video}'
+        )
+    await update.message.reply_text(message)
+
+
 def create_app() -> telegram.ext.Application:
     '''Create the bot application.'''
     logger.info('Creating app')
@@ -37,4 +78,11 @@ def create_app() -> telegram.ext.Application:
         save,
     )
     app.add_handler(save_handler)
+
+    # on command "/hacker" send a message to the users
+    app.add_handler(telegram.ext.CommandHandler("hacker", hacker))
+
+    # on command "/hackear" send a message to the users
+    app.add_handler(telegram.ext.CommandHandler("hackear", hackear))
+
     return app
