b5b095be230af16812128c406c550dc753329e27
Edinho <git@final04.com>
2023-11-19 13:43:21 -0300

    feat: Change logging config to json

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import json
 import logging
 import logging.config
 
 from chuvisco import main
 
-logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logging.config.dictConfig(json.load(open('logging.json')))
 logger = logging.getLogger(__name__)
 
 # Run the bot until the user presses Ctrl-C
